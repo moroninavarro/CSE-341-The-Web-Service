61462498f30cf8817ca723f518c01ea14f06b5be
moroninavarro <moronialanis23@gmail.com>
2025-11-14 16:57:56 -0800

    change

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API',
     description: 'Temple API'
   },
-  host: 'cse341-instructor.herokuapp.com',
+  host: 'cse-341-the-web-service-1.onrender.com',
   schemes: ['https']
 };
 
